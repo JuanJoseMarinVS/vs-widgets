9e138648a1de1c14d1f6b79fc444e0f0e4f3bc79
JuanJoseMarinVS <juan.marin@virtualsoft.tech>
2026-02-10 09:42:05 -0500

    feat: add Tailwind CSS and PostCSS configuration, update project files

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts';
-import { resolve } from 'path';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      name: 'VsWidget',
-      fileName: 'lib',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'lib/main.ts'),
+            name: 'VsWidget',
+            fileName: 'lib',
         },
-      },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
     },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        },
     },
-  },
-  plugins: [vue(), dts({ rollupTypes: true })],
-})
+    plugins: [vue(), dts({ rollupTypes: true })],
+});
